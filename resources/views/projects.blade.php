3f56e09e6bc92db77ffde3e215a29804d3f8b996
Majakamal <kamal.maja@yahoo.com>
2020-06-22 12:33:06 -0700

    master

---
+++ resources/views/projects.blade.php
@@ -0,0 +1,18 @@
+  projects
+  @extends('layouts.layout')
+
+  @section('content')
+        <div class="flex-center position-ref full-height">
+           
+            <div class="content">
+                <div class="title m-b-md">
+                    Project list
+                </div>
+           <p>{{ $type }} - {{$base}} - {{$cost}}</p>
+
+           
+            </div>
+        </div>
+   @endsection
+
+            
